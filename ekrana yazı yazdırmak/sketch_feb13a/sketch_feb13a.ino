f74a4bc73f5c39d961a019fff2a3a99a5e9750c7
Ömer Faruk Koç <77229286+Fcyb3r@users.noreply.github.com>
2021-02-13 15:30:50 +0300

    Create sketch_feb13a.ino

---
+++ ekrana yazı yazdırmak/sketch_feb13a/sketch_feb13a.ino
@@ -0,0 +1,15 @@
+void setup() {
+Serial.begin(9600); // haberleşme hızı
+}
+
+void loop() {
+ 
+ Serial.println("ömer"); //aktarmak istedigimiz veriyi/verileri yazdık 
+  Serial.println("---------------------");
+ Serial.println("fcyber ");
+  Serial.println("---------------------");
+ Serial.println("35"); 
+
+
+ delay(1000); // Bir saniye bekle
+}
